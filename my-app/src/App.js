8bd93029bd2f507d5a110714ea3b60d4b1ff399f
wudl <wudl@kaopuyun.com>
2018-12-19 18:37:19 +0800

    add:my-app新增项目说明

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 	render() {
-		var a;
+		var b;
 		return (
 			<div className='App'>
 				<header className='App-header'>
