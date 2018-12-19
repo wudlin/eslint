2aaee7e4dc0d1310ff2166e31ad3367e947771f7
wudl <wudl@kaopuyun.com>
2018-12-19 18:25:29 +0800

    update:修改导出config文件

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 class App extends Component {
 	render() {
-    var a;
-    
-
-    
+		var a;
 		return (
 			<div className='App'>
 				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
+					<p>一个简单的 eslint + prettier 规范下的项目</p>
 					<a
 						className='App-link'
-						href='https://reactjs.org'
+						href='https://github.com/wudlin/eslint'
 						target='_blank'
 						rel='noopener noreferrer'
 					>
-						Learn React
+						Learn eslint --prettier
 					</a>
 				</header>
 			</div>
