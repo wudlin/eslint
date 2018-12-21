ce6b589da1f2923fd0ef0b00b6550a5c659aeec0
wudl <wudl@kaopuyun.com>
2018-12-21 14:42:59 +0800

    test:s

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,15 +10,15 @@ class App extends Component {
 		// console.log('测试log警告影响打包');
 		return (
 			<div className='App'>
-				<header className="App-header">
-					<p>一个简单的 eslint + prettier 规范下的项目</p>
+				<header className='App-header'>
+					<p>一个简单的应用 eslint + prettier 规范的项目</p>
 					<a
 						className='App-link'
 						href='https://github.com/wudlin/eslint'
 						target='_blank'
 						rel='noopener noreferrer'
 					>
-						Learn eslint --prettier
+                        Learn eslint --prettier
 					</a>
 				</header>
 			</div>
