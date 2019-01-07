08f9e26e825cafc93cf8eeab5954d27209264898
wudl <wudl@kaopuyun.com>
2019-01-07 11:28:04 +0800

    test:测试跳过验证

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,28 +5,24 @@ class App extends Component {
 		super(props);
 		this.state = {};
 	}
-    a = () => {
-    	//ss
-    	console.log(1111);
-    };
-    render() {
-    	// var b;
-    	// console.log('测试log警告影响打包');
-    	return (
-    		<div className='App'>
-    			<header className='App-header'>
-    				<p>一个简单的应用 eslint + prettier 规范的项目</p>
-    				<a
-    					className='App-link'
-    					href='https://github.com/wudlin/eslint'
-    					target='_blank'
-    					rel='noopener noreferrer'
-    				>
-                        Learn eslint --prettier
-    				</a>
-    			</header>
-    		</div>
-    	);
-    }
+	render() {
+		// var b;
+		// console.log('测试log警告影响打包');
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<p>一个简单的应用 eslint + prettier 规范的项目</p>
+					<a
+						className='App-link'
+						href='https://github.com/wudlin/eslint'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						Learn eslint --prettier
+					</a>
+				</header>
+			</div>
+		);
+	}
 }
 export default App;
